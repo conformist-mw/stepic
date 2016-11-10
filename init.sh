89b3cdc7906fda36eee539c2c91427fd233809bc
conformist-mw <conformist.mw@gmail.com>
2016-11-10 16:18:52 +0200

    stage 3

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nignx.conf
 sudo /etc/init.d/nginx restart
-mkdir web/{public,uploads}
 sudo rm /etc/gunicorn.d/*
-sudo ln -s /home/box/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn.conf
+sudo ln -s /home/box/etc/gunicorn-django.conf /etc/gunicorn.d/django.conf
+sudo ln -s /home/box/etc/gunicorn-wsgi.conf /etc/gunicorn.d/wsgi.conf
 sudo pip3 install django gunicorn
 sudo sed -i  -e '1s/python/python3/' -e 's/17.5/19.6.0/g' /usr/sbin/gunicorn-debian \
 /usr/bin/gunicorn /usr/bin/gunicorn-django /usr/bin/gunicorn-paster
