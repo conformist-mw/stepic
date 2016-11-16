d3e63d8e374d1cd3825765d5e1e7ba600826e904
conformist-mw <conformist.mw@gmail.com>
2016-11-16 22:40:35 +0200

    af

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,15 +15,16 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateField()
     rating = models.IntegerField()
-    author = models.OneToOneField(User)
-    likes = models.ManyToManyField(User, through='Likes')
+    author = models.ForeignKey(User, related_name='question_autor')
+    likes = models.ManyToManyField(User, through='Likes',
+                                   related_name='question_likes')
 
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField()
-    question = models.ForeignKey(Question)
-    author = models.OneToOneField(User)
+    question = models.ForeignKey(Question, related_name='question_answer')
+    author = models.ForeignKey(User, related_name='answer_autor')
 
 
 class Likes(models.Model):
